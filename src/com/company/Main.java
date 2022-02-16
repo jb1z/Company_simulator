3e1d12cdfb39960bab5875ee233fda35a90de08d
jbiz <pozolotusiko@mail.ru>
2022-02-17 01:28:22 +0300

    Some moves with layouts

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,36 +3,43 @@ package com.company;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
 public class Main extends Application{
-
     public static void main(String[] args) {
         launch(args);
     }
 
     public void start(Stage stage) {
+        StackPane root = new StackPane();
         Button startButton = new Button("Start");
-        startButton.setPrefWidth(80);
         Label nameLabel = new Label();
+        StackPane.setAlignment(nameLabel, Pos.TOP_LEFT);
         Label moneyLabel = new Label();
+        StackPane.setAlignment(moneyLabel, Pos.TOP_LEFT);
+        StackPane.setMargin(moneyLabel, new Insets(20.0, 0.0, 0.0, 0.0));
         Label valueLabel = new Label();
-        startButton.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                Company mainCompany = new Company();
-                nameLabel.setText("Company's name: " + mainCompany.getName());
-                moneyLabel.setText("Company's money:" + String.valueOf(mainCompany.money) + "$");
-                valueLabel.setText("Value: " + String.valueOf(mainCompany.value) + "$");
-            }
+        StackPane.setAlignment(valueLabel, Pos.TOP_LEFT);
+        StackPane.setMargin(valueLabel, new Insets(40.0, 0.0, 0.0, 0.0));
+        root.getChildren().addAll(startButton, nameLabel, moneyLabel, valueLabel);
+        startButton.setOnAction(event -> {
+            Company mainCompany = new Company();
+            nameLabel.setText("Company's name: " + mainCompany.getName());
+            moneyLabel.setText("Company's money:" + mainCompany.money + "$");
+            valueLabel.setText("Value: " + mainCompany.value + "$");
+            startButton.setVisible(false);
         });
-        FlowPane root = new FlowPane(startButton, nameLabel, moneyLabel, valueLabel);
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("JavaFX Application");
+        stage.setTitle("Company");
         stage.setWidth(1000);
         stage.setHeight(500);
         stage.show();
